df060df152d4397afd10c9f21ef631a99c3e91d5
rgrgzz <ict_701@naver.com>
2025-12-14 21:45:19 +0900

    fix: swaggerconfig 비활성화 테스트

--- src/main/java/com/bookstore/api/config/SwaggerConfig.java
+++ src/main/java/com/bookstore/api/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@Configuration
+// @Configuration
 public class SwaggerConfig {
 
     @Value("${server.port:8080}")

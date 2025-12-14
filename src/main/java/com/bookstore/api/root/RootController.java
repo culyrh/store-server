30b85581afdb63264505b517cd5d7acb30826046
rgrgzz <ict_701@naver.com>
2025-12-14 13:46:10 +0900

    feat: jcloud 포트 연결 완료

---
+++ src/main/java/com/bookstore/api/root/RootController.java
@@ -0,0 +1,13 @@
+package com.bookstore.api.root;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "Bookstore API Server is running";
+    }
+}

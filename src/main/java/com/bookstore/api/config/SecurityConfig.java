98e1e1c4016de3761cafb5594f56be2e1cb24276
rgrgzz <ict_701@naver.com>
2025-12-14 11:36:22 +0900

    feat: Swagger 접근 문제 해결(spring security)

--- src/main/java/com/bookstore/api/config/SecurityConfig.java
+++ src/main/java/com/bookstore/api/config/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.bookstore.api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Swagger + OpenAPI + Actuator 포함 전부 허용
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
 }
